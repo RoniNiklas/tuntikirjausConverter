7f73273d91502e518ba7ada8a29e7168577e7b52
Roni Uusoksa <Roni.Uusoksa@syke.fi>
2023-05-25 20:53:03 +0300

    stuff

--- tuntikirjaukset/Program.cs
+++ tuntikirjaukset/Program.cs
@@ -1,17 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ExcelDataReader;
 using System.Globalization;
 using System.Text;
 
-const char COLUMN_SEPARATOR = ';';
-const string INPUT_FILENAME = "Tuntiraportti.csv";
+const string INPUT_FILENAME = "Tuntiraportti.xlsx";
 var INPUT_FILEPATH = AppDomain.CurrentDomain.BaseDirectory + "\\" + INPUT_FILENAME;
 const string OUTPUT_FILENAME = "Tuntiraportti_projekteittain.csv";
 var OUTPUT_FILEPATH = AppDomain.CurrentDomain.BaseDirectory + "\\" + OUTPUT_FILENAME;
 const double WORKDAY_LENGTH = 7.25;
 
-var values = File.ReadLines(INPUT_FILEPATH, Encoding.Latin1)
-    .Skip(1)
-    .Select(v => TuntikirjausData.FromCsv(v, COLUMN_SEPARATOR))
+List<TuntikirjausData> tuntikirjausData = new();
+
+using (var stream = File.Open(INPUT_FILEPATH, FileMode.Open, FileAccess.Read)) {
+    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance); // needed for .net core compatability
+    using var reader = ExcelReaderFactory.CreateReader(stream);
+    reader.Read(); // skip header row
+    while (reader.Read()) {
+        tuntikirjausData.Add(TuntikirjausData.FromReader(reader));
+    }
+}
+
+var groupedValues = tuntikirjausData
     .GroupBy(e => new {
         e.Toimintayksikkö,
         e.Project,
@@ -19,18 +28,18 @@ var values = File.ReadLines(INPUT_FILEPATH, Encoding.Latin1)
     })
     .Select(e => new OutputData(
         e.Key.Toimintayksikkö,
-        e.First().ToimintayksikköName, // For some reason, the same toimintayksikkö can have multiple names
+        e.First().ToimintayksikköName, // For some reason, the same toimintayksikkö can have multiple names (basically just typoes of what it's supposed to be)
         e.Key.Toiminto,
         e.First().ToimintoName, // The same toiminto does not have multiple names, but just for consistency's sake it's not used as key
         e.Key.Project,
-        e.First().ProjectName, // For some reason, the same project can have multiple names
+        e.First().ProjectName, // For some reason, the same project can have multiple names (basically just typoes of what it's supposed to be)
         double.Round(e.ToList().Sum(e => e.AllocatedHours.TotalHours), 2),
         double.Round(e.ToList().Sum(e => e.AllocatedHours.TotalHours) / WORKDAY_LENGTH, 2))) // too lazy to not sum it twice
     .OrderBy(e => e.Project)
     .ToList();
 
 var csvHeaderRow = "ToimintaYksikkö;ToimintaYksikköName;Toiminto;ToimintoName;Project;ProjectName;AllocatedHours;AllocatedDays" + "\n";
-File.WriteAllText(OUTPUT_FILEPATH, csvHeaderRow + string.Join("\n", values), Encoding.Latin1);
+File.WriteAllText(OUTPUT_FILEPATH, csvHeaderRow + string.Join("\n", groupedValues), Encoding.Latin1);
 
 public record OutputData(string ToimintaYksikkö, string ToimintaYksikköName, string Toiminto, string ToimintoName, string Project, string ProjectName, double AllocatedHours, double AllocatedDays) {
     public override string ToString() {
@@ -57,29 +66,38 @@ public record TuntikirjausData(
     string Seuko2Name, // always empty
     string Explanation, // always empty
     string State) {
-
-    public static TuntikirjausData FromCsv(string csvLine, char columnSeparator) {
-        var values = csvLine.Split(columnSeparator);
+    public static TuntikirjausData FromReader(in IExcelDataReader reader) {
         var dailyValues = new TuntikirjausData(
-            ContractNumber: values[0],
-            Title: values[1],
-            Date: DateTime.Parse(values[2], CultureInfo.GetCultureInfo("fi")),
-            AllocatedHours: new TimeSpan(int.Parse(values[3].Split(":")[0]), int.Parse(values[3].Split(":")[1]), 0),
-            Toimintayksikkö: values[4],
-            ToimintayksikköName: values[5],
-            Toiminto: values[6],
-            ToimintoName: values[7],
-            Project: values[8],
-            ProjectName: values[9],
-            Suorite: values[10],
-            SuoriteName: values[11],
-            Seuko1: values[12],
-            Seuko1Name: values[13],
-            Seuko2: values[14],
-            Seuko2Name: values[15],
-            Explanation: values[16],
-            State: values[17]
+            ContractNumber: reader.FromCell(0),
+            Title: reader.FromCell(1),
+            Date: DateTime.Parse(reader.FromCell(2), CultureInfo.GetCultureInfo("fi")),
+            AllocatedHours: new TimeSpan(int.Parse(reader.FromCell(3).Split(":")[0]), int.Parse(reader.FromCell(3).Split(":")[1]), 0),
+            Toimintayksikkö: reader.FromCell(4),
+            ToimintayksikköName: reader.FromCell(5),
+            Toiminto: reader.FromCell(6),
+            ToimintoName: reader.FromCell(7),
+            Project: reader.FromCell(8),
+            ProjectName: reader.FromCell(9),
+            Suorite: reader.FromCell(10),
+            SuoriteName: reader.FromCell(11),
+            Seuko1: reader.FromCell(12),
+            Seuko1Name: reader.FromCell(13),
+            Seuko2: reader.FromCell(14),
+            Seuko2Name: reader.FromCell(15),
+            Explanation: reader.FromCell(16),
+            State: reader.FromCell(17)
         );
         return dailyValues;
+    }
+}
+
+public static class IExcelDataReaderExtensions {
+    public static string FromCell(this IExcelDataReader reader, in int column) {
+        try {
+            return reader.GetString(column);
+        }
+        catch (Exception e) {
+            return "";
+        }
     }
 }
